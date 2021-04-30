91f7c8ea5585091c50b9186e323df4e7a2a6a2bd
keyjey101 <keyjey.danilov@gmail.com>
2021-04-30 17:34:53 +0500

    third

--- db.js
+++ db.js
@@ -16,18 +16,12 @@ port: process.env.DB_PORT
 
 const prod = new Sequelize (
 
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
     {
-dialect: 'postgres',
-host: process.env.DB_HOST,
-port: process.env.DB_PORT,
-connectionString: process.env.DATABASE_URL
+connectionString: "postgres://sgcwvpaokvtzwl:d6882a02751e9fe70bfd4e1c83417a3521e547d33d8cea5cbf79886ee349263b@ec2-52-21-252-142.compute-1.amazonaws.com:5432/d8a936cciknf3v"
     }
 
 )
 
 
-module.exports = process.env.NODE_ENV === 'production' ? prod : dev
+module.exports = process.env.NODE_ENV === 'development' ? dev : prod
 
