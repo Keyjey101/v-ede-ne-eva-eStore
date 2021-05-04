d5fa0156b48e07df2750a31b8cd4b5a77d96c9e6
keyjey101 <keyjey.danilov@gmail.com>
2021-05-04 18:22:57 +0500

    testibg

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const Sequelize = require('sequelize').Sequelize
+const Sequelize = require('sequelize')
 
 module.exports = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
