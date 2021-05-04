a1139d758cbe8c28bf1d76f5a9f84fc0c6d9c0e1
keyjey101 <keyjey.danilov@gmail.com>
2021-05-04 17:46:02 +0500

    test2

--- db.js
+++ db.js
@@ -3,13 +3,10 @@ const Sequelize = require('sequelize').Sequelize
 
 module.exports = new Sequelize (
 
-    'd8a936cciknf3v',
-    'sgcwvpaokvtzwl',
-    'd6882a02751e9fe70bfd4e1c83417a3521e547d33d8cea5cbf79886ee349263b',
+   
     {
 dialect: 'postgres',
-host: 'ec2-52-21-252-142.compute-1.amazonaws.com',
-port: 5432
+use_env_variable: 'DATABASE_URL'
     }
 
 )
