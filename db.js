c3ee2184f2f5799a779a0f97001e81076d4d2c6c
keyjey101 <keyjey.danilov@gmail.com>
2021-05-04 18:45:47 +0500

    frfer

--- db.js
+++ db.js
@@ -1,15 +1,20 @@
 require('dotenv').config()
 const Sequelize = require('sequelize')
 
-module.exports = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    protocol: 'postgres',
+module.exports = new Sequelize({
+    database: "d8a936cciknf3v",
+    username: "sgcwvpaokvtzwl",
+    password: "d6882a02751e9fe70bfd4e1c83417a3521e547d33d8cea5cbf79886ee349263b",
+    host: "ec2-52-21-252-142.compute-1.amazonaws.com",
+    port: 5432,
+    dialect: "postgres",
     dialectOptions: {
-        ssl: true,
-        rejectUnauthorized: false
-    }
-
-})
+      ssl: {
+        require: true,
+        rejectUnauthorized: false // <<<<<<< YOU NEED THIS
+      }
+    },
+  });
 
 
 
