ffd1c302140ff85cbb0ee055a59f7175084dc07a
keyjey101 <keyjey.danilov@gmail.com>
2021-05-04 17:51:48 +0500

    test db

--- db.js
+++ db.js
@@ -1,17 +1,14 @@
 require('dotenv').config()
 const Sequelize = require('sequelize').Sequelize
 
-module.exports = new Sequelize (
-
-   
-    {
-dialect: 'postgres',
-use_env_variable: 'DATABASE_URL'
+module.exports = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: true
     }
 
-)
-
-
+})
 
 
 
